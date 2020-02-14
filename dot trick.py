7026c53304b72945cbc41db91ad09b77f62d049f
bpnrockstar <bipinrai.rai123@gmail.com>
2020-02-14 09:48:46 -0800

    Add files via upload

---
+++ dot trick.py
@@ -0,0 +1,22 @@
+def dot_trick(username):
+    emails = list()
+    username_length = len(username)
+    combinations = pow(2, username_length - 1)
+    padding = "{0:0" + str(username_length - 1) + "b}"
+    for i in range(0, combinations):
+        bin = padding.format(i)
+        full_email = ""
+
+        for j in range(0, username_length - 1):
+            full_email += (username[j]);
+            if bin[j] == "1":
+                full_email += "."
+        full_email += (username[j + 1])
+        emails.append(full_email + "@gmail.com")
+    return emails
+
+
+
+username = input("Give Username(eg. kate344mail) = ")
+print(*dot_trick(username) , sep="\n")
+ex = input("Done, Press ctrl + c to exit")
